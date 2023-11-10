4cbdee56235244dce6d8f9c21073dfdd8ec1dd79
dmmontoto <dmontoto@ucsd.edu>
2023-11-10 15:02:03 -0800

    added footer with links on all pages

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+function Footer() {
+  
+    return (
+        <div>
+        <a href="https://www.linkedin.com/in/david-montoto" target="_blank" rel="noreferrer">
+          <img src="/src/images/LinkedIn.png" alt="LinkedIn"/>
+        </a>
+        <a href="https://www.github.com/dmmontoto" target="_blank" rel="noreferrer">
+          <img src="/src/images/GitLogo.png" alt="GitHub"/>
+        </a>
+        <a href="https://www.instagram.com/david_montoto/" target="_blank" rel="noreferrer">
+          <img src="/src/images/Instagram.png" alt="Instagram"/>
+        </a>
+      </div>
+    );
+  }
+  
+  export default Footer;
+  
