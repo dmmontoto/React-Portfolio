0863b3dda9f92b88791f6b8995f40b3989213e9b
dmmontoto <dmontoto@ucsd.edu>
2023-11-11 10:41:57 -0800

    put css for landing page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,14 @@
 export default function Home() {
   return (
-    <div>
-      <h1>Home Page</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-        velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-        ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-        non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-        ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-        rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-        tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-        porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-        vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-        fames ac ante ipsum primis in faucibus.
-      </p>
+    <section className="d-flex flex-column justify-content-center align-items-center fs-1" id ="landing">
+    <div id="home">
+      <h6>Hi, my name is</h6>
+      <h1>David Montoto</h1>
+      <h2>I build things for the web</h2>
+      <p>I am a software engineer hailing from sunny Southern California and proudly studying at the University of California,
+        San Diego. With a strong foundation in web projects,
+        I bring expertise in advanced data structures and backend development to create impactful solutions.</p>
     </div>
+  </section>
   );
 }
