6e5a68c05252d71aed5cbe69bac4560ad5f86bc0
dmmontoto <dmontoto@ucsd.edu>
2023-11-11 11:20:33 -0800

    rocket works and redesigning clock on about me

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,32 @@
+import { useEffect } from 'react';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 export default function About() {
+  useEffect(() => {
+    function stars() {
+      let count = 50;
+      let scene = document.querySelector('.rocket-scene');
+      let i = 0;
+      while (i < count) {
+        let star = document.createElement('i');
+        let x = Math.floor(Math.random() * window.innerWidth);
+
+        let duration = Math.random() * 1;
+        let h = Math.random() * 100;
+
+        star.style.left = x + 'px';
+        star.style.width = 1 + 'px';
+        star.style.height = h + 'px';
+        star.style.animationDuration = duration + 's';
+
+        scene.appendChild(star);
+        i++;
+      }
+    }
+
+    // Call the stars function when the component mounts
+    stars();
+  }, []);
   return (
     <div>
       <section className="about-me">
@@ -10,7 +38,7 @@ export default function About() {
           <div className="col-md-6">
             <div className="rocket-scene">
               <div className="rocket">
-                <img className="rocket-img" src="assets/images/rocket.png"/>
+                <img className="rocket-img" src="src/images/rocket.png"/>
               </div>
             </div>
           </div>
@@ -53,53 +81,29 @@ export default function About() {
             <div className="clock-background" id="clock-background">
               <div className="clock">
 
-                <div className="circle" id="sc" style="--clr: #c11515;"><i></i></div>
-                <div className="circle circle2" id="mn" style="--clr: #175b93;"><i></i></div>
-                <div className="circle circle3" id="hr" style="--clr: #000000;"><i></i></div>
+                <div className="circle" id="sc" style={{ '--clr': '#c11515' }}><i></i></div>
+                <div className="circle circle2" id="mn" style={{ '--clr': '#175b93' }}><i></i></div>
+                <div className="circle circle3" id="hr" style={{ '--clr': '#000000' }}><i></i></div>
 
-                <span style="--i:1;"><b>1</b></span>
-                <span style="--i:2;"><b>2</b></span>
-                <span style="--i:3;"><b>3</b></span>
-                <span style="--i:4;"><b>4</b></span>
-                <span style="--i:5;"><b>5</b></span>
-                <span style="--i:6;"><b>6</b></span>
-                <span style="--i:7;"><b>7</b></span>
-                <span style="--i:8;"><b>8</b></span>
-                <span style="--i:9;"><b>9</b></span>
-                <span style="--i:10;"><b>10</b></span>
-                <span style="--i:11;"><b>11</b></span>
-                <span style="--i:12;"><b>12</b></span>
+                <span style={{ '--i': 1 }}><b>1</b></span>
+                <span style={{ '--i': 2 }}><b>2</b></span>
+                <span style={{ '--i': 3 }}><b>3</b></span>
+                <span style={{ '--i': 4 }}><b>4</b></span>
+                <span style={{ '--i': 5 }}><b>5</b></span>
+                <span style={{ '--i': 6 }}><b>6</b></span>
+                <span style={{ '--i': 7 }}><b>7</b></span>
+                <span style={{ '--i': 8 }}><b>8</b></span>
+                <span style={{ '--i': 9 }}><b>9</b></span>
+                <span style={{ '--i': 10 }}><b>10</b></span>
+                <span style={{ '--i': 11 }}><b>11</b></span>
+                <span style={{ '--i': 12 }}><b>12</b></span>
               </div>
 
               <div id="time">
-                <div id="hour" style="--clr: #000000;">00</div>
-                <div id="minutes" style="--clr: #175b93;">00</div>
-                <div id="seconds" style="--clr: #c11515;">00</div>
+                <div id="hour" style={{ '--clr': '#000000' }}>00</div>
+                <div id="minutes" style={{ '--clr': '#175b93' }}>00</div>
+                <div id="seconds" style={{ '--clr': '#c11515' }}>00</div>
                 <div id="ampm">AM</div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="container">
-        <div className="row">
-          <div className="col-md-6">
-
-          </div>
-          <div className="col-md-6">
-            <div className="glass-container">
-              <div className="glass-card">
-                <div className="glass-content">
-                  <h2>A Pathway to Impact</h2>
-                  <p>Over the past year, I have relentlessly pursued knowledge and skill enhancement, diving deep into software engineering. I have explored data structures, version control, 
-                    server architecture, and web development. My toolkit includes languages like Javascript, C++, Java, C, Python, ARM Assembly, R, and MATLAB, each providing a unique lens 
-                    for tackling challenges. With frameworks like Node.js, Express.js, and React.js, I have built proficiency in the MERN stack. Tools like Sequelize, Git, and Visual Studio Code 
-                    enable collaborative project development. I have mastered HTML, CSS, Bootstrap, JavaScript, DOM manipulation, APIs, JQuery, JSON, and AJAX, creating engaging user experiences. 
-                    This journey is perpetual, presenting daily opportunities to learn and innovate. My commitment goes beyond mastery—it is about using these skills to create impactful change in 
-                    the real world, a promise I am dedicated to fulfilling.
-                  </p>
-                </div>
               </div>
             </div>
           </div>
