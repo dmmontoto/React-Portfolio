87a38cace8437316770d3dc1a32382d5d80e6805
dmmontoto <dmontoto@ucsd.edu>
2023-11-12 11:53:40 -0800

    added clock css and functionality method

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -27,6 +27,55 @@ export default function About() {
     // Call the stars function when the component mounts
     stars();
   }, []);
+
+  useEffect(() => {
+    function clock() {
+      let hr = document.querySelector('#hr');
+      let mn = document.querySelector('#mn');
+      let sc = document.querySelector('#sc');
+
+      setInterval(() => {
+        let day = new Date();
+        let hour = day.getHours() * 30;
+        let minute = day.getMinutes() * 6;
+        let second = day.getSeconds() * 6;
+
+        hr.style.transform = `rotateZ(${hour+(minute/12)}deg)`;
+        mn.style.transform = `rotateZ(${minute}deg)`;
+        sc.style.transform = `rotateZ(${second}deg)`;
+
+        // Digital
+        let hours = document.getElementById('hour');
+        let minutes = document.getElementById('minutes');
+        let seconds = document.getElementById('seconds');
+        let ampm = document.getElementById('ampm');
+
+        let h = new Date().getHours();
+        let m = new Date().getMinutes();
+        let s = new Date().getSeconds();
+
+        let am = h >= 12 ? "PM" : "AM";
+
+        // converting 24hr clock to 12hr
+        if (h > 12) {
+            h = h - 12;
+        }
+
+        // add zero before single digit number
+        h = (h < 10) ? "0" + h : h;
+        m = (m < 10) ? "0" + m : m;
+        s = (s < 10) ? "0" + s : s;
+
+        hours.innerHTML = h;
+        minutes.innerHTML = m;
+        seconds.innerHTML = s;
+        ampm.innerHTML = am;
+      })
+    }
+
+    clock();
+  }, []);
+
   return (
     <div>
       <section className="about-me">
