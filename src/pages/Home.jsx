c64dd8abdfaaadd4d9ece02530db2b781b3efa61
dmmontoto <dmontoto@ucsd.edu>
2023-11-13 17:34:00 -0800

    implementing infinity wheel on landing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,76 @@
 export default function Home() {
   return (
     <section className="d-flex flex-column justify-content-center align-items-center fs-1" id ="landing">
-    <div id="home">
-      <h6>Hi, my name is</h6>
-      <h1>David Montoto</h1>
-      <h2>I build things for the web</h2>
-      <p>I am a software engineer hailing from sunny Southern California and proudly studying at the University of California,
-        San Diego. With a strong foundation in web projects,
-        I bring expertise in advanced data structures and backend development to create impactful solutions.</p>
-    </div>
-  </section>
+      <div id="home">
+        <h6>Hi, my name is</h6>
+        <h1>David Montoto</h1>
+        <h2>I build things for the web</h2>
+        <p>I am a software engineer hailing from sunny Southern California and proudly studying at the University of California,
+          San Diego. With a strong foundation in web projects,
+          I bring expertise in advanced data structures and backend development to create impactful solutions.</p>
+        <div>
+          <div>
+            <span>Javascript</span>
+            <span>React.js</span>
+            <span>Node.js</span>
+            <span>Express.js</span>
+            <span>C++</span>
+            <span>Python</span>
+            <span>Web APIs</span>
+            <span>UIs</span>
+            <span>Java</span>
+            <span>C</span>
+            <span>SQL</span>
+            <span>MongoDB</span>
+            <span>API Development</span>
+            <span>MVC Architecture</span>
+            <span>HTML</span>
+            <span>CSS</span>
+            <span>NoSQL</span>
+            <span>ARM</span>
+            <span>MATLAB</span>
+            <span>R</span>
+            <span>Git</span>
+            <span>Sequelize</span>
+            <span>Pandas</span>
+            <span>MERN Stack</span>
+            <span>GraphQL</span>
+            <span>DOM</span>
+            <span>Handlebars.js</span>
+            <span>JSON</span>
+          </div>
+          <div>
+            <span>Javascript</span>
+            <span>React.js</span>
+            <span>Node.js</span>
+            <span>Express.js</span>
+            <span>C++</span>
+            <span>Python</span>
+            <span>Web APIs</span>
+            <span>UIs</span>
+            <span>Java</span>
+            <span>C</span>
+            <span>SQL</span>
+            <span>MongoDB</span>
+            <span>API Development</span>
+            <span>MVC Architecture</span>
+            <span>HTML</span>
+            <span>CSS</span>
+            <span>NoSQL</span>
+            <span>ARM</span>
+            <span>MATLAB</span>
+            <span>R</span>
+            <span>Git</span>
+            <span>Sequelize</span>
+            <span>Pandas</span>
+            <span>MERN Stack</span>
+            <span>GraphQL</span>
+            <span>DOM</span>
+            <span>Handlebars.js</span>
+            <span>JSON</span>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
