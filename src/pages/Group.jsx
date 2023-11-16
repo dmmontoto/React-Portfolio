51862aacff84d748150ada8720368c1277607986
dmmontoto <dmontoto@ucsd.edu>
2023-11-15 17:33:18 -0800

    adding major ui improvement to projects page

--- src/pages/Group.jsx
+++ src/pages/Group.jsx
@@ -5,16 +5,21 @@ export default function Group() {
       <div>
         <div className="container">
           <div className="row">
-            <div className="col-md-6">
+            <div className="col-md-6" id='Project-Container'>
               <div className='Project-Card' style={{ '--clr': '#009688' }}>
                 <div className='imgBx'></div>
                 <div className='Project-Content'></div>
               </div>
             </div>
-            <div className="col-md-6">
+            <div className="col-md-6" id='Project-Container'>
               <div className='Project-Card' style={{ '--clr': '#ff3e7f' }}>
-                <div className='imgBx'></div>
-                <div className='Project-Content'></div>
+                <div className='imgBx'>
+                  <img src='/src/images/EazyExplorer.png'></img>
+                </div>
+                <div className='Project-Content'>
+                  <h2>Eazy Explorer</h2>
+                  <p>this is a test to see how the text looks inside of the project contect portion of project page</p>
+                </div>
               </div>
             </div>
           </div>
@@ -22,13 +27,13 @@ export default function Group() {
 
         <div className="container">
           <div className="row">
-            <div className="col-md-6">
+            <div className="col-md-6" id='Project-Container'>
               <div className='Project-Card' style={{ '--clr': '#009688' }}>
-                <div className='imgBx'></div>
+                <div className="imgBx"></div>
                 <div className='Project-Content'></div>
               </div>
             </div>
-            <div className="col-md-6">
+            <div className="col-md-6" id='Project-Container'>
               <div className='Project-Card' style={{ '--clr': '#009688' }}>
                 <div className='imgBx'></div>
                 <div className='Project-Content'></div>
