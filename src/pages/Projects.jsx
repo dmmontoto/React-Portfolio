75e98c8ec41c11c0e276b8d3133117b248b2fa17
dmmontoto <dmontoto@ucsd.edu>
2023-11-25 11:47:51 -0800

    worked on book search project

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -111,18 +111,18 @@ export default function Projects() {
                   <h3>Account-Based Book Finder</h3>
                   <h5>Javascript, Node.js, Express.js, SQL, OpenAI API, Heroku, Github</h5>
                   <ul id="bullets">
-                    <li>Developed a dynamic tech blog platform with user accounts, blog post creation, and interactive commenting features</li>
-                    <li>Designed a responsive web interface using HTML, CSS, and JavaScript, ensuring a seamless user experience</li>
-                    <li>Implemented secure SQL database for efficient account management and post storage, enhancing data accessibility and user control</li>
+                    <li>Successfully modified authentication middleware to seamlessly integrate with the GraphQL API while implementing an Apollo Provider for effective communication with the Apollo Server</li>
+                    <li>Utilized React for the frontend, delivering an intuitive and user-friendly interface that aligns with user expectations, implementing dynamic menu changes based on user authentication status</li>
+                    <li>Demonstrated a commitment to continuous improvement by adhering to best practices, adopting GraphQL for enhanced data querying, and ensuring a user-centric approach throughout the development process</li>
                   </ul>
                   <div style={contactSectionStyle}>
-                    <a href="https://eazy-explorer-8c2ae355679a.herokuapp.com/" target="_blank" rel="noreferrer" onMouseEnter={handleWebsiteHover} onMouseLeave={handleWebsiteLeave}>
+                    <a href="https://online-book-search-7ae70d0dd885.herokuapp.com/" target="_blank" rel="noreferrer" onMouseEnter={handleWebsiteHover} onMouseLeave={handleWebsiteLeave}>
                       <img src="/src/images/WebsiteImage.png" alt="Website" style={{
                       ...imageStyle,
                       ...(isWebsiteHovered && imageHoverStyle),
                       }}/>
                     </a>
-                    <a href="https://github.com/dmmontoto/Eazy-Explorer" target="_blank" rel="noreferrer" onMouseEnter={handleGitHubHover} onMouseLeave={handleGitHubLeave}>
+                    <a href="https://github.com/dmmontoto/Book-Search-Engine" target="_blank" rel="noreferrer" onMouseEnter={handleGitHubHover} onMouseLeave={handleGitHubLeave}>
                     <img src="/src/images/GitLogo.png" alt="GitHub" style={{
                     ...imageStyle,
                     ...(isGitHubHovered && imageHoverStyle),
@@ -200,25 +200,25 @@ export default function Projects() {
             <div className="col-md-6" id='Project-Container'>
               <div className='Project-Card Bottom-Card' style={{ '--clr': '#009688' }}>
                 <div className='imgBx'>
-                <img src='/src/images/GP1slidePreview.gif'></img>
+                <img src='/src/images/TechBlog.png'></img>
                 </div>
                 <div className='Project-Content'>
                 <h2>Technology Blog</h2>
                   <h3>Account-Based Blog Site</h3>
                   <h5>Javascript, Movie API, Recipe API, JSON, Github</h5>
                   <ul id="bullets">
-                    <li>Collaborated with classmates Marissa and Huaquin to devise an innovative method for pairing movies and recipes to enhance the overall entertainment experience</li>
-                    <li>Demonstrated proficiency in integrating two distinct APIs, one for movie searching and another for recipe retrieval, ensuring seamless data integration and retrieval</li>
-                    <li>Assumed a leadership role in spearheading the development of the JavaScript framework, overseeing its architecture and implementation to ensure a robust and efficient solution</li>
+                    <li>Developed a dynamic tech blog platform with user accounts, blog post creation, and interactive commenting features</li>
+                    <li>Designed a responsive web interface using HTML, CSS, and JavaScript, ensuring a seamless user experience</li>
+                    <li>Established a secure SQL database for efficient account management and post storage, prioritizing data accessibility and user control</li>
                   </ul>
                   <div style={contactSectionStyle}>
-                    <a href="https://marissacraig.github.io/MovieMunchies/index.html" target="_blank" rel="noreferrer" onMouseEnter={handleWebsiteHover} onMouseLeave={handleWebsiteLeave}>
+                    <a href="https://account-based-tech-blog-30c1584537a5.herokuapp.com" target="_blank" rel="noreferrer" onMouseEnter={handleWebsiteHover} onMouseLeave={handleWebsiteLeave}>
                       <img src="/src/images/WebsiteImage.png" alt="Website" style={{
                       ...imageStyle,
                       ...(isWebsiteHovered && imageHoverStyle),
                       }}/>
                     </a>
-                    <a href="https://github.com/marissacraig/MovieMunchies" target="_blank" rel="noreferrer" onMouseEnter={handleGitHubHover} onMouseLeave={handleGitHubLeave}>
+                    <a href="https://github.com/dmmontoto/Account-Based-Tech-Blog" target="_blank" rel="noreferrer" onMouseEnter={handleGitHubHover} onMouseLeave={handleGitHubLeave}>
                     <img src="/src/images/GitLogo.png" alt="GitHub" style={{
                     ...imageStyle,
                     ...(isGitHubHovered && imageHoverStyle),
