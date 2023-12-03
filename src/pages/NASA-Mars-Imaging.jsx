b7c1f470efb75268a89a2383141b1e1a1b32b502
dmmontoto <dmontoto@ucsd.edu>
2023-12-03 00:56:14 -0800

    added nasa mars imaging page without web api call

--- src/pages/NASA-Mars-Imaging.jsx
+++ src/pages/NASA-Mars-Imaging.jsx
@@ -1,30 +1,100 @@
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 export default function Imaging() {
-    return (
-      <main>
-        <section id="plotter-container">
-          <div id="glass-card" className="row justify-content-center align-items-center">
-            <div>
-              <div className="buttons d-flex justify-content-center align-items-center flex-column">
-                <h1>Python Rocket Data Plotter</h1>
-              </div>
-              <div id="plotter-images">
-                <img src="./assets/Hot-fire.png" alt="Hot Fire Rocket" />
-                <img src="./assets/Cold-flow.png" alt="Cold Flow" /> 
-              </div>
-              <div id="plotter-bullets">
-                <ul>
-                <li>Created interactive data visualizations using Python libraries such as Matplotlib and Pandas, allowing users to explore rocket telemetry data dynamically and gain valuable insights</li>
-                <li>Implemented rigorous data validation and cleaning processes to ensure the accuracy and reliability of the telemetry data, resulting in more dependable visualizations and analyses</li>
-                <li>Designed and optimized the Python script to efficiently handle and process large CSV files, enabling swift data analysis and visualization for a wide range of rocket telemetry datasets</li>
-                <li>Prepared clear and user-friendly documentation, including instructions for script usage, making it easy for team members and collaborators to understand and utilize the project effectively</li>
-                </ul>
-              </div>
+  return (
+    <main className="mars-background" id="nasa-api">
+
+      <div id="nasaLogo">
+        <img id="nasa-img" src="assets/NASA.png" />
+      </div>
+
+      <section className="nasa-container justify-content-center align-items-center">
+        <div id="search" className="row justify-content-center align-items-center">
+          <div className="col-lg-8 col-md-10 col-sm-12">
+            <div className="buttons d-flex justify-content-center flex-lg-row flex-column">
+              <button id="curiosity" className="btn btn-primary">
+                Curiosity
+              </button>
+              <button id="spirit" className="btn btn-primary">
+                Spirit
+              </button>
+              <button id="opportunity" className="btn btn-primary">
+                Opportunity
+              </button>
             </div>
-          </div>  
-        </section>
-      </main>
-    );
-  }
-  
+            <h2 id="search-title" className="text-center">
+              Search Images
+            </h2>
+            <p id="search-description" className="text-center">
+              Please select a rover
+            </p>
+            <div
+              id="selectors"
+              className="d-flex flex-wrap justify-content-center align-items-center"
+            >
+              <input
+                type="date"
+                id="date-pattern"
+                className="form-control me-2 mb-2"
+              />
+              <input
+                id="rover-camera-input"
+                list="cameras"
+                className="form-control mb-2"
+                type="text"
+                placeholder="Rover Cameras"
+              />
+              <datalist id="cameras">
+                <option value="Front Hazard Avoidance Camera"></option>
+                <option value="Rear Hazard Avoidance Camera"></option>
+                <option value="Mast Camera"></option>
+                <option value="Chemistry and Camera Complex"></option>
+                <option value="Mars Hand Lens Imager"></option>
+                <option value="Mars Descent Imager"></option>
+                <option value="Navigation Camera"></option>
+                <option value="Panoramic Camera"></option>
+                <option value="Miniature Thermal Emission Spectrometer"></option>
+              </datalist>
+            </div>
+            <h3 id="search-alert"></h3>
+            <div
+              id="selectors"
+              className="d-flex flex-wrap justify-content-center align-items-center"
+            >
+              <button id="search-btn" className="btn btn-primary">
+                Search
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section id="response">
+        <h2 id="response-title"></h2>
+        <div className="row">
+          <div className="col-lg-6">
+            <div id="rover-details">
+              <p id="rover-description"></p>
+              <iframe
+                id="youtube-iframe"
+                width="100%"
+                height="315"
+                src=""
+                allowfullscreen
+              ></iframe>
+            </div>
+          </div>
+          <div className="col-lg-6">
+            <div
+              id="slideshow"
+              className="carousel slide"
+              data-bs-ride="carousel"
+            >
+              <div id="slideshow-inner" className="carousel-inner"></div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+}
